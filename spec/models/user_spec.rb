018d9313f455fdb201f12c02045aa73fbd53e552
FlorianSahling <florian.sahling@prod.uni-hannover.de>
2013-12-05 10:58:28 +0100

    Exercises in Chapter 6 done

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,6 +45,16 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
